f99bebc81f6d6e9669dc57ae5e550b7cbb282566
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2016-05-05 15:13:40 +0100

    Supplements tests for util module

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,13 +1,34 @@
 const expect = require('chai').expect;
 const hat = require('../index');
 
-const inputData = require('./fixtures/inputObject');
+const dataSourceModel = require('./fixtures/dataModel');
+const inputObject = require('./fixtures/inputObject');
+const inputArray = require('./fixtures/multipleInputObjects');
 const hatIdMapping = require('./fixtures/hatIdMapping');
-const expectedResult = require('./fixtures/expectedHatRecord');
+const expectedHatRecord = require('./fixtures/hatRecord');
+const expectedMultipleHatRecords = require('./fixtures/multipleHatRecords');
 
 describe('Data transformation utility functions', function() {
-  it('convert single nested object to HAT data structure', function() {
-    const result = hat.transform.transformObjToHat(inputData, hatIdMapping, 'givenString');
-    expect(result).to.deep.equal(expectedResult);
+  it('extracts HAT field ID mapping from HAT data model', function() {
+    const result = hat.transform.mapDataSourceModelIds(dataSourceModel);
+    expect(result).to.deep.equal(hatIdMapping);
+  });
+
+  it('throws error for invalid HAT data model', function() {
+    try {
+      hat.transform.mapDataSourceModelIds({ id: 1, name: "events" });
+    } catch(err) {
+      expect(err).to.eql(new Error);
+    }
+  });
+
+  it('convert single nested object to a HAT data structure', function() {
+    const result = hat.transform.transformObjToHat(inputObject, hatIdMapping, 'givenString');
+    expect(result).to.deep.equal(expectedHatRecord);
+  });
+
+  it('convert array of objects to a HAT data structure', function() {
+    const result = hat.transform.transformObjToHat(inputArray, hatIdMapping, 'givenString');
+    expect(result).to.deep.equal(expectedMultipleHatRecords);
   });
 });
